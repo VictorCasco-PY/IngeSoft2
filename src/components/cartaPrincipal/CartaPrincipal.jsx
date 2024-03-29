ee386f8296405df5807a71275a034596aa7328af
Fernando Brizuela <ffba97@gmail.com>
2024-03-28 23:29:20 -0400

    Sin loader

--- src/components/cartaPrincipal/CartaPrincipal.jsx
+++ src/components/cartaPrincipal/CartaPrincipal.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import "./CartaPrincipal.css";
-import CircularProgress from "@mui/material/CircularProgress";
 
-const loading = () => (<div className="loading">
-<CircularProgress /> <br/>
-<div>Cargando...</div>
-</div>)
-
-const CartaPrincipal = ({ children, isCentered = true, isLoading }) => {
+const CartaPrincipal = ({ children, isCentered = true }) => {
   return (
     <>
-      <div className='carta rounded-3 shadow shadow-lg'>
-          {isLoading ? loading() : children}
+      <div className='carta rounded-3  shadow-lg'>
+          {children}
       </div>
     </>
   )
