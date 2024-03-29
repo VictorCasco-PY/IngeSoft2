4d431ecc3f0d3ba5805190a55dd8f5a952106ec0
arvidjj <andy.oj14@gmail.com>
2024-03-28 20:08:19 -0400

    Fixed the table

--- src/components/tablas/TablaActividadesCliente.jsx
+++ src/components/tablas/TablaActividadesCliente.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import useSuscripcion from '../../hooks/useSuscripcion';
 import CircularProgress from "@mui/material/CircularProgress";
+import { useNavigate } from 'react-router-dom';
+import './TablaActividades.css';
+import { capFirstMinRest, formatFecha } from '../../utils/Formatting';
 
 const TablaActividadesCliente = ({ toast, clienteId }) => {
+
     const { getSuscripcionesByCliente, data: req_suscripciones, isLoading: cargandoSuscripciones, error: errorSuscripciones } = useSuscripcion();
     const [actividades, setActividades] = useState([]);
-
     const [loadTable, setLoadTable] = useState(false);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -26,11 +31,6 @@ const TablaActividadesCliente = ({ toast, clienteId }) => {
         fetchData();
     }, []);
 
-
-    const capFirstMinRest = (string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-    }
-
     return (
         <table class="table table-striped">
             <thead>
@@ -39,6 +39,7 @@ const TablaActividadesCliente = ({ toast, clienteId }) => {
                     <th>Modalidad</th>
                     <th>Estado</th>
                     <th>Fecha de Inscripcion</th>
+                    <th>Fecha de Vencimiento</th>
                 </tr>
             </thead>
             <tbody>
@@ -51,10 +52,13 @@ const TablaActividadesCliente = ({ toast, clienteId }) => {
                 ) : (
                     actividades['items'].map((actividad) => (
                         <tr key={actividad.id}>
-                            <td>{actividad.actividadNombre}</td>
+                            <td onClick={() => { navigate(`/infoServicio/${actividad.actividadId}`) }} className='rowClickable'>
+                                {actividad.actividadNombre}
+                            </td>
                             <td>{capFirstMinRest(actividad.modalidad)}</td>
                             <td>{capFirstMinRest(actividad.estado)}</td>
-                            <td>{actividad.fechaInicio}</td>
+                            <td>{formatFecha(actividad.fechaInicio)}</td>
+                            <td>{formatFecha(actividad.fechaFin)}</td>
                         </tr>
                     ))
                 )}
