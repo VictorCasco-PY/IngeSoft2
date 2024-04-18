ab7c86f4bfc6dd0a180b772e6d3365fa161f0c9e
Fernando Brizuela <ffba97@gmail.com>
2024-04-18 15:57:21 -0400

    Añadido ids

--- src/pages/caja/FacturaModal.jsx
+++ src/pages/caja/FacturaModal.jsx
@@ -27,7 +27,6 @@ export const FacturaModal = ({ open, closeModal, data, guardar }) => {
 
 
     return <ModalBase title={`Factura Nº ${factura?.factura?.nroFactura}`} open={open} closeModal={closeModal}>
-        {console.log(factura)}
         <div>
             <p className='py-2'>
                 <b>Fecha de emision:</b> {factura?.factura?.fecha}<br />
@@ -57,9 +56,9 @@ export const FacturaModal = ({ open, closeModal, data, guardar }) => {
 
 
             <div className="d-flex justify-content-center align-items-center float-end mt-4 gap-3">
-                <Btn onClick={closeModal} type="secondary">Cerrar</Btn>
+                <Btn id="btn-cerrar" onClick={closeModal} type="secondary">Cerrar</Btn>
                 {guardar && <Btn type="secondary" onClick={() => guardar()} outline>Guardar Factura</Btn>}
-                {factura?.factura?.saldo ? <Btn type="primary" onClick={() => setCobroModal(true)}>Cobrar Factura</Btn> : "" }
+                {factura?.factura?.saldo ? <Btn id="btn-cobrar-factura" type="primary" onClick={() => setCobroModal(true)}>Cobrar Factura</Btn> : "" }
             </div>
 
         </div>
