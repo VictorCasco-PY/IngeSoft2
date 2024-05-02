6eed43c6eceb43092200f86c918f688d8b9f5899
arvidjj <andy.oj14@gmail.com>
2024-05-02 14:09:59 -0400

    headers

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      "Strict-Transport-Security": "max-age=86400; includeSubDomains", 
-      "X-Content-Type-Options": "nosniff",
-      "X-Frame-Options": "DENY",
-      "X-XSS-Protection": "1; mode=block", 
-      'Content-Security-Policy': 'upgrade-insecure-requests',
+      "strict-transport-security": "max-age=86400; includeSubDomains", 
+      "referrer-policy": "same-origin", 
+      "x-frame-options": "DENY",
+      "permissions-policy": "geolocation=(self)", 
     },
   },
 })
