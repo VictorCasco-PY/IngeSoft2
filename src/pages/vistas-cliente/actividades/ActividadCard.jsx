e56aa6ce73db69a65fc85407f57bddc87b4c7ec7
MarceloDanielFerreira <113811868+MarceloDanielFerreira@users.noreply.github.com>
2024-05-16 14:40:29 -0400

    Update ActividadCard.jsx
    Se agrego un ID

--- src/pages/vistas-cliente/actividades/ActividadCard.jsx
+++ src/pages/vistas-cliente/actividades/ActividadCard.jsx
@@ -6,7 +6,7 @@ const ActividadCard = ({ actividad, onClick }) => {
       className="col-12 col-md-6 col-lg-4 mb-3"
       onClick={() => onClick(actividad)}
     >
-      <div className="card">
+      <div className="card" id={`card${actividad.nombre}`}>
         <div className="card-body">
           <h5 className="card-title">{actividad.nombre}</h5>
           <p className="card-text">{actividad.descripcion}</p>
