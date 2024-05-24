37e0c0fe12bcc96e0046ce58e915c83cd6a57118
Fernando Brizuela <ffba97@gmail.com>
2024-05-24 17:03:37 -0400

    Verificando si el token es valido

--- src/main.jsx
+++ src/main.jsx
@@ -47,6 +47,7 @@ import MainDashboardCliente from "./pages/dashboard/dashboardCliente/MainDashboa
 import MainDashboardEntrenador from "./pages/dashboard/dashboardEntrenador.jsx/MainDashboadEntrenador";
 import { SugerirCambioPassword } from "./pages/sugerir-cambio-contraseña/SugerirCambioPassword";
 import RecuperarContraseña from "./pages/recuperar-contraseña/RecuperarContrasenha";
+import { VerifyToken } from "./utils/VerifyToken";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -392,6 +393,7 @@ createRoot(document.getElementById("root")).render(
               }
             />
         </Routes>
+        <VerifyToken />
       </CurrentUserProvider>
     </Router>
   </React.StrictMode>
---
+++ src/utils/VerifyToken.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "./api";
+import { useCurrentUser } from "../context/UserContext";
+import toast, { Toaster } from "react-hot-toast";
+export const VerifyToken = () => {
+
+    const [valid, setValid] = useState(true);
+
+    const navigate = useNavigate();
+    const { token, logout } = useCurrentUser();
+
+
+
+    const tokenExpired = () => {
+        if (valid) {
+            toast.error("Su sesión ha expirado, por favor inicie sesión nuevamente.")
+        }
+        removeTokenAndGoToLogin()
+    }
+
+    const removeTokenAndGoToLogin = () => {
+        logout();
+        navigate("/");
+        setValid(false);
+    }
+
+
+    useEffect(() => {
+        const verifyToken = async () => {
+            try {
+                // Ya el token se envía en la cabecera de la petición, no es necesario enviarlo en el body
+                const res = await api.get("/auth/verify");
+                console.log(res)
+                console.lo("AAAAAAAAAAAAAAAAAA")
+                if (res.data === "true") {
+                    return;
+                }
+            } catch (error) {
+                if (error.response.status === 403 || error.response.status === 401) {
+                    tokenExpired();
+                }
+            }
+        }
+
+        verifyToken();
+    }, []);
+
+    return (<>
+        <Toaster
+            position="top-right"
+            reverseOrder={false}
+            toastOptions={{
+                success: {
+                    style: {
+                        background: "#75B798",
+                        color: "#0A3622",
+                    },
+                },
+                error: {
+                    style: {
+                        background: "#FFDBD9",
+                        color: "#D92D20",
+                    },
+                },
+            }}
+        />
+
+    </>)
+
+}
